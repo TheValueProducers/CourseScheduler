46ab00f4316a9621c61e701797bed56bdca99cde
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2026-05-25 11:56:43 +0700

    added env variables

--- backend/main.py
+++ backend/main.py
@@ -74,6 +74,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "https://course-scheduler-nu.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
